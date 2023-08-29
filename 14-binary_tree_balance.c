cc28ee809f8d594d79124ec03e3f0e6f34dbe382
Anzelah <jepkoechanzellah@gmail.com>
2023-08-29 07:24:38 -0700

    find height first then call it

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,32 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_hheight - find tree height first
+ * @tree: pointer to the root node of the tree to measure the height
+ *
+ * Return: If tree is NULL, return 0
+ */
+
+size_t binary_tree_hheight(const binary_tree_t *tree)
+{
+	size_t lefth = 0;
+	size_t righth = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left)
+		lefth = 1 + binary_tree_hheight(tree->left);
+	if (tree->right)
+		righth = 1 + binary_tree_hheight(tree->right);
+
+	if (lefth > righth)
+		return (lefth);
+	else
+		return (righth);
+}
+
+/**
  * binary_tree_balance - measure the balance factor of a binary tree
  * @tree: pointer to the root node of the tree to measure the balance factor
  *
@@ -9,18 +35,17 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int lefth = 0;
-	int righth = 0;
-	int balance;
+	int left = 0;
+	int right = 0;
+	int balance = 0;
 
 	if (tree == NULL)
 		return (0);
-
 	if (tree->left)
-		lefth = 1 + binary_tree_balance(tree->left);
+		left = 1 + binary_tree_hheight(tree->left);
 	if (tree->right)
-		righth = 1 + binary_tree_balance(tree->right);
+		right = 1 + binary_tree_hheight(tree->right);
+	balance = (left - right);
 
-	balance = (lefth - righth);
 	return (balance);
 }
