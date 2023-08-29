a510eb9469ac67a5469d0f38eb179c803448a03e
Anzelah <jepkoechanzellah@gmail.com>
2023-08-28 22:57:29 -0700

    preorder traversal

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -13,7 +13,7 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree == NULL || *func == NULL)
 		return;
 
-	printf("%d ->", tree->n);
-	binary_tree_preorder(tree->left, (func)(int n));
-	binary_tree_preorder(tree->right, (func)(int n));
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
